14e4bc838df56d9a311179fd45012eeced4e4ca9
Karanpreet Kaur <karanpreet088@gmail.com>
2022-02-05 12:15:08 -0800

    Edit trim outliers with a fix

--- R/trim_outliers.R
+++ R/trim_outliers.R
@@ -20,60 +20,64 @@
 #'                   )
 #'
 #'trim_outliers(df,identifier='Z_score', method='trim')
-trim_outliers <- function(dataframe, columns=NULL, identifier='Z_score', method='trim') {
-  if (!is.data.frame(dataframe)) {
-    stop("The argument dataframe is not dataframe")
+trim_outliers <- function(dataframe, columns=NULL, identifier='IQR', method='trim') {
+  if(!is.data.frame(dataframe)){
+    stop(paste0("passed dataframe is of type", typeof(dataframe), ", should be DataFrame"))
   }
-  # check if there is non-numeric column
-  if (is.null(columns)) {
-    if (sum(unlist(lapply(dataframe, is.numeric))) != length(dataframe)) {
-      stop("there is non-numeric column in dataframe")
-    }
-  } else {
-    if (!is.vector(columns)) {
-      stop("Argument columns must be a vector")
-    }
-    possible_numeric_col <-
-      dataframe %>% select_if(is.numeric) %>% colnames()
-    for (col in columns) {
-      if (!(col %in% possible_numeric_col)) {
-        stop("The given columns contains non-numeric columns in this dataframe.")
-      }
-    }
+
+  if(is.na.data.frame(dataframe)[1] == TRUE){
+    stop(paste0("passed dataframe is None"))
+  }
+
+  if((!is.null(columns)) & (!is.character(columns))){
+    stop(paste0("passed columns is of type", typeof(columns), ", should be character vector or NULL"))
+  }
+
+  if((!is.character(identifier))){
+    stop(paste0("passed identifier is of type", typeof(identifier), ", should be character string with value 'Z_score' or 'IQR'"))
+  }
+
+  if(identifier!='Z_score' & identifier!='IQR'){
+    stop(paste0("passed identifier should have value 'Z_score' or 'IQR'"))
   }
   # check if method input is valid
   if (!method %in% c("trim", "median", "mean")) {
     stop("The given method must be one of ('trim', 'median', 'mean')")
   }
-  df_cp <- data.frame(dataframe)
-  if (is.null(columns)) {
-    columns <- names(df_cp)
+  df_cp <- dataframe
+
+  if(is.null(columns)){
+    columns <-  names(dataframe)
   }
-  outlier_index <- c()
-  for (col in columns) {
-    current_col <- df_cp[col]
-    col_mean <- apply(current_col, 2, mean)
-    col_std <- apply(current_col, 2, sd)
-    threshold <- 2.5
-    for (i in 1:nrow(df_cp)) {
-      current_item <- current_col[i, ]
-      z = (current_item - col_mean) / col_std
-      if (z >= threshold) {
-        if (!i %in% outlier_index) {
-          outlier_index <- c(outlier_index, i)
-        }
-        if (method == "mean") {
-          df_cp[i, col] = round(col_mean, 2)
-        }
-        if (method == "median") {
-          df_cp[i, col] = apply(current_col, 2, median)
-        }
-      }
+  df_filtered <-  dataframe |>
+    select(columns)
+  numeric_columns = names(select_if(df_filtered, is.numeric))
+
+  df = outlier_identifier(df_filtered, columns=numeric_columns, identifier=identifier, return_df = TRUE)
+
+  if(method == 'trim'){
+    outlier_index <- c(rownames(df[which(df$outlier == TRUE), ]))
+    if(length(outlier_index) >= 1){
+      df_cp <- df_cp[-which(df$outlier == TRUE), ]
     }
+
   }
-  if (method == "trim" & !is.null(outlier_index)) {
-    df_cp <- df_cp[-outlier_index,]
+
+  else if(method == 'mean'){
+    for(col in numeric_columns){
+      df_col = outlier_identifier(dataframe, columns=col, identifier=identifier, return_df = TRUE)
+      outlier_index <- c(rownames(df_col[which(df_col$outlier == TRUE), ]))
+      df_cp[outlier_index, ][col] = round(mean(dataframe[[col]], na.rm = TRUE), 2)
+    }
+
   }
-  row.names(df_cp) <- NULL
+  else if(method == 'median'){
+    for(col in numeric_columns){
+      df_col = outlier_identifier(dataframe, columns=col, identifier=identifier, return_df = TRUE)
+      outlier_index <- c(rownames(df_col[which(df_col$outlier == TRUE), ]))
+      df_cp[outlier_index, ][col] = round(median(dataframe[[col]], na.rm = TRUE), 2)
+    }
+
+  }
   return(df_cp)
 }
--- tests/testthat/test_trim_outliers.R
+++ tests/testthat/test_trim_outliers.R
@@ -1,109 +1,32 @@
-test_df = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 4.7, 5.5, 5.1, 50, 5.4, 5.0, 5.2, 5.3, 5.1),
-  SepalWidthCm = c(1.4, 1.4, 20, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
-  PetalWidthCm  = c(0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 5),
-  Species = c(
-    'Iris-setosa',
-    'Iris-virginica',
-    'Iris-germanica',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa'
-  )
-)
-
-num_df = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 4.7, 5.5, 5.1, 50, 5.4, 5.0, 5.2, 5.3, 5.1),
-  SepalWidthCm = c(1.4, 1.4, 20, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
-  PetalWidthCm  = c(0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 5)
-)
+test_df = data.frame(SepalLengthCm = c(5.2, 4.9, 4.7, 5.5, 5.1, 5.0, 6.0, 54, 50, 5.3, 5.1),
+                     SepalWidthCm = c(-5.4, 1.4, -5.7, 0.2, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
+                     PetalWidthCm = c(-40, 0.2, 0.2, 0.3, 0.2, 0.5, 0.5, 0.6, 0.4, 0.2, 5),
+                     class = c('Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Virginica',
+                               'Iris Setosa', 'Iris Versicolour', 'Iris Setosa', 'Iris Versicolour'))
 
 test_column = c('SepalLengthCm', 'SepalWidthCm', 'PetalWidthCm')
 
-median_output = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 4.7, 5.5, 5.1, 5.1, 5.4, 5.0, 5.2, 5.3, 5.1),
-  SepalWidthCm = c(1.4, 1.4, 1.5, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
-  PetalWidthCm  = c(0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 0.4),
-  Species = c(
-    'Iris-setosa',
-    'Iris-virginica',
-    'Iris-germanica',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa'
-  )
-)
+median_output = data.frame(SepalLengthCm = c(5.2, 4.9, 4.7, 5.5, 5.1, 5.0, 6.0, 5.2, 5.2, 5.3, 5.1),
+                           SepalWidthCm = c(1.4, 1.4, 1.4, 0.2, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
+                           PetalWidthCm = c(0.3, 0.2, 0.2, 0.3, 0.2, 0.5, 0.5, 0.6, 0.4, 0.2, 0.3),
+                           class = c('Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Virginica',
+                                     'Iris Setosa', 'Iris Versicolour', 'Iris Setosa', 'Iris Versicolour'))
 
-trim_output = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 5.5, 5.1, 5.4, 5.0, 5.2, 5.3),
-  SepalWidthCm = c(1.4, 1.4, 2.0, 0.7, 1.2, 1.4, 1.8, 1.5),
-  PetalWidthCm  = c(0.2, 0.2, 0.3, 0.4, 0.5, 0.6, 0.4, 0.2),
-  Species = c(
-    'Iris-setosa',
-    'Iris-virginica',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa'
-  )
-)
-
-num_out = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 4.7, 5.5, 5.1, 9.21, 5.4, 5.0, 5.2, 5.3, 5.1),
-  SepalWidthCm = c(1.4, 1.4, 3.19, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
-  PetalWidthCm  = c(0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 0.77)
-)
+trim_output = data.frame(SepalLengthCm = c(4.9, 5.5, 5.1, 5.0, 6.0, 5.3),
+                         SepalWidthCm = c(1.4, 0.2, 0.7, 1.6, 1.2, 1.5),
+                         PetalWidthCm = c(0.2, 0.3, 0.2, 0.5, 0.5, 0.2),
+                         class = c('Iris Versicolour', 'Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Virginica',
+                                   'Iris Setosa'),
+                         row.names = c('2','4', '5', '6', '7', '10'))
 
 
-mean_output = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 4.7, 5.5, 5.1, 9.21, 5.4, 5.0, 5.2, 5.3, 5.1),
-  SepalWidthCm = c(1.4, 1.4, 3.19, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
-  PetalWidthCm  = c(0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 0.77),
-  Species = c(
-    'Iris-setosa',
-    'Iris-virginica',
-    'Iris-germanica',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa'
-  )
-)
 
-column_output = data.frame(
-  SepalLengthCm = c(5.1, 4.9, 4.7, 5.5, 5.1, 9.21, 5.4, 5.0, 5.2, 5.3, 5.1),
-  SepalWidthCm = c(1.4, 1.4, 20, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
-  PetalWidthCm  = c(0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 5),
-  Species = c(
-    'Iris-setosa',
-    'Iris-virginica',
-    'Iris-germanica',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa',
-    'Iris-setosa'
-  )
-)
+mean_output = data.frame(SepalLengthCm = c(5.2, 4.9, 4.7, 5.5, 5.1, 5.0, 6.0, 13.71, 13.71, 5.3, 5.1),
+                         SepalWidthCm = c(0.07, 1.4, 0.07, 0.2, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1),
+                         PetalWidthCm = c(-2.9, 0.2, 0.2, 0.3, 0.2, 0.5, 0.5, 0.6, 0.4, 0.2, -2.9),
+                         class = c('Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Setosa', 'Iris Versicolour', 'Iris Virginica', 'Iris Virginica',
+                                   'Iris Setosa', 'Iris Versicolour', 'Iris Setosa', 'Iris Versicolour'))
+
 
 # dataframe argument test
 test_that("Dataframe arument must be a data.frame", {
@@ -121,13 +44,6 @@ test_that(
   }
 )
 
-# data contains non-numeric columns
-test_that(
-  "non-numeric columns in the data frame",
-  {
-    expect_error(trim_outliers(test_df, method = "mean"))
-  }
-)
 # method argument test
 test_that("selected method is not one of c('trim', 'median', 'mean')", {
   expect_error(trim_outliers(test_df, columns = test_column, method = "bro"))
@@ -137,9 +53,10 @@ test_that("selected method is not one of c('trim', 'median', 'mean')", {
 # test for value equal
 test_that("The outlier function has error values", {
   expect_true(all.equal(
-    trim_outliers(test_df, columns = test_column),
-    trim_output
+    rownames(trim_outliers(test_df, columns = test_column)),
+    rownames(trim_output)
   ))
+
   expect_true(all.equal(
     trim_outliers(test_df, columns = test_column, method = "median"),
     median_output
@@ -147,20 +64,5 @@ test_that("The outlier function has error values", {
   expect_true(all.equal(
     trim_outliers(test_df, columns = test_column, method = "mean"),
     mean_output
-  ))
-  expect_true(all.equal(
-    trim_outliers(
-      test_df,
-      columns = c("SepalLengthCm"),
-      method = "mean"
-    ),
-    column_output
-  ))
-  expect_true(all.equal(
-    trim_outliers(
-      num_df,
-      method = "mean"
-    ),
-    num_out
   ))
 })
